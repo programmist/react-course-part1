ba3062516fe577b7b27cc30af6794416e0d09bca
Tony Childs <tony.childs@gmail.com>
2024-05-26 15:55:01 -0500

    Conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,23 @@
 function ListGroup() {
-  const items = ["New York", "London", "Tokyo", "Paris", "Geneva"];
+  let items = [
+    { id: 1, city: "New York" },
+    { id: 2, city: "London" },
+    { id: 3, city: "Tokyo" },
+    { id: 4, city: "Paris" },
+    { id: 5, city: "Geneva" },
+  ];
+
+  items = [];
+
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No Items Found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item">{item}</li>
+        {items.map(({ id, city }) => (
+          <li key={id} className="list-group-item">
+            {city}
+          </li>
         ))}
       </ul>
     </>
