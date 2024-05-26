4964b60db646af96355ad8b66bb08c3a4588c6c6
Tony Childs <tony.childs@gmail.com>
2024-05-26 15:54:30 -0500

    Dynamic list rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,12 @@
 function ListGroup() {
+  const items = ["New York", "London", "Tokyo", "Paris", "Geneva"];
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li className="list-group-item">{item}</li>
+        ))}
       </ul>
     </>
   );
