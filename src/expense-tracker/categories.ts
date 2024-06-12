fc8911fdb2f5e9e0b9fb3720d0f0ce2bcfb0f587
Tony Childs <tony.childs@gmail.com>
2024-06-11 19:56:43 -0500

    Validate ExpenseForm with zod schema

--- src/expense-tracker/categories.ts
+++ src/expense-tracker/categories.ts
@@ -1,7 +1,22 @@
-export type ExpenseCategory = "Groceries" | "Utilities" | "Entertainment";
+import { z } from "zod";
+
+export const ExpenseCategoryTypeSchema = z.union([
+  z.literal("Groceries"),
+  z.literal("Utilities"),
+  z.literal("Entertainment"),
+]);
+
+// Zod union doesn't allow mapping from array in a non-convoluted, readable
+// manner so I'm going the other way and mapping union to string array
+// https://github.com/colinhacks/zod/issues/831
+export const categoryStrings = [
+  ...ExpenseCategoryTypeSchema._def.options.map((opt) => opt.value),
+] as const;
+
+export type ExpenseCategory = (typeof categoryStrings)[number];
 
 export const expenseCategories: Array<ExpenseCategory> = [
   "Groceries",
   "Utilities",
   "Entertainment",
-];
+] as const;
