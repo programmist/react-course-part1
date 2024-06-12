8c5d07a45bbf9fbf8d34eaffce98807658f0523a
Tony Childs <tony.childs@gmail.com>
2024-06-12 15:48:27 -0500

    Add spinner

--- src/apps/FetchDataApp.tsx
+++ src/apps/FetchDataApp.tsx
@@ -19,26 +19,34 @@ interface User {
 function GetUsersApp() {
   const [data, setData] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     // Reset
     setData([]);
     setError("");
 
+    setLoading(true);
     const controller = new AbortController();
-    axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
-        signal: controller.signal,
-      })
-      .then((res) => setData(res.data))
-      .catch((err) =>
-        err instanceof CanceledError ? null : setError(err.message)
-      );
+
+    // simulate a delay
+    setTimeout(() => {
+      axios
+        .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+          signal: controller.signal,
+        })
+        .then((res) => setData(res.data))
+        .catch((err) =>
+          err instanceof CanceledError ? null : setError(err.message)
+        )
+        .finally(() => setLoading(false));
+    }, 1000);
 
     return () => controller.abort();
   }, []);
   return (
     <>
+      {isLoading && <div className="spinner-border"></div>}
       {error && <p className="text-danger">{error}</p>}
       <ul>
         {data.map((user) => (
